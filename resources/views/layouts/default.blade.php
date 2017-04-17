a076e2ff583aa5b2f358e7f3c2fa5a69b8628079
lichuncheng <lichuncheng101631@icloud.com>
2017-04-17 23:50:08 +0800

    finish_static_pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>@yield('title','Sample App') - Laravel入门教程</title>
+  <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+  <header class="navbar navbar-fixed-top navbar-inverse">
+
+  <h1>@yield('content')</h1>
+</body>
+</html>
