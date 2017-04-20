a059ecb0c8a172b26e3592890b0fccff7f937123
lichuncheng <lichuncheng101631@icloud.com>
2017-04-20 23:03:42 +0800

    add cnpm node-sass yarn gulp bootstrap-sass laravel-elixir

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>@yield('title','Sample App') - Laravel入门教程</title>
-  <link rel="stylesheet" href="/css/app.css">
-</head>
-<body>
-  <header class="navbar navbar-fixed-top navbar-inverse">
-
-  <h1>@yield('content')</h1>
-</body>
+  <head>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    <header class="navbar navbar-fixed-top navbar-inverse">
+      <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+          <a href="/" id="logo">Sample App</a>
+          <nav>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="/help">帮助</a></li>
+              <li><a href="#">登录</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      @yield('content')
+    </div>
+  </body>
 </html>
